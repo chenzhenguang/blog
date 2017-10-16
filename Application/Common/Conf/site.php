a0fc2c607d4a6aab8b5d7e87f8b3946ff137c14f
chenzhenguang <458893084@qq.com>
2017-10-16 10:19:48 +0800

    初始化博客项目

---
+++ Application/Common/Conf/site.php
@@ -0,0 +1,11 @@
+<?php 
+return array (
+  'sitename' => '轮回博客后台管理系统',
+  'keyword' => '轮回博客',
+  'content' => '网站内容管理',
+  'siteurl' => 'http://tianjianlong.com.cn',
+  'copyright' => '<a href="http://www.miitbeian.gov.cn/" class="foot-my" target="_blank">陇ICP备15002349号</a>',
+  'qq' => '864491238',
+  'linktel' => '18809316782',
+  'address' => 'Copyright © 2014 - 2016  轮回博客  & 版权所有',
+);
